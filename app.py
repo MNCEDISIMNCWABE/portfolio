34be530a0a05e2e4edcc3b0aeb1a8dc4fe2ab7f1
Mncedisi Mncwabe <67195600+MNCEDISIMNCWABE@users.noreply.github.com>
2023-03-22 15:17:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -163,6 +163,22 @@ with st.container():
             """
         )
         st.markdown("[Deployed Web App Link...](https://sp-preds-predictions-mod-app-aqcjue.streamlit.app/)")
+        
+ with st.container():
+    image_column, text_column = st.columns((1, 2))
+    with image_column:
+        st.image(img_contact_form)
+    with text_column:
+        st.subheader("Customer Purchase Propensity Modelling")
+        st.write(
+            """
+           Propensity Modelling is a method of generating a probability score on how customers do a particular action. 
+           In this exercise I explore a combination of Propensity Analysis with RFM scores (Recency, Frequency, Monetary) 
+           to predict the probability score of a customer buying a product after a campaign was done.
+            """
+        )
+        st.markdown("[Propensity Modelling...](https://github.com/MNCEDISIMNCWABE/exploring-ml-and-analytics/tree/main/Propensity%20Modelling)")                   
+              
 with st.container():
     image_column, text_column = st.columns((1, 2))
     with image_column:
